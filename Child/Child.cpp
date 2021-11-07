0d728067c67cbcd860f166d19f9158ca4b8122d2
maximkurochka <kurochka.maxim@gmail.com>
2021-11-07 19:27:28 +0200

    Implement transfering of handler from parent to child process

---
+++ Child/Child.cpp
@@ -0,0 +1,29 @@
+﻿// Child.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <boost/mpi/environment.hpp>
+#include <boost/mpi/communicator.hpp>
+#include <Windows.h>
+namespace mpi = boost::mpi;
+
+int main()
+{
+	mpi::environment env;
+	mpi::communicator world;
+
+	std::cout << "The child process executes" << std::endl;
+
+	if (!world.rank())
+	{
+		std::cout << "The parent process does not exist with mpi type communication" << std::endl;
+		return 0;
+	}
+
+	HANDLE hFileShared;
+	world.recv(0, 1, hFileShared);
+
+	std::cout << "The value of handler:" << hFileShared << std::endl;
+
+	return 0;
+}
