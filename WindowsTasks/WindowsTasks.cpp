1c815576408f83f25a1ba1b840880fa8d610445e
maximkurochka <kurochka.maxim@gmail.com>
2021-11-07 19:45:15 +0200

    Implemetn TaskContainer class, that resposnsible for association proceses to one task

--- WindowsTasks/WindowsTasks.cpp
+++ WindowsTasks/WindowsTasks.cpp
@@ -1,9 +1,20 @@
 ﻿// WindowsTasks.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+//Chapter 5
+//Процесс порождает два дочерних процесса, объединённых в одну задачу.
 
 #include <iostream>
+#include "TaskContainer.h"
+
+void CreateTwoChildProcessInOneTask()
+{
+	TaskContainer task;
+	task.AddProcess();
+	task.AddProcess();
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::cout << "Association of two processes in one task" << std::endl;
+	CreateTwoChildProcessInOneTask();
+	return 0;
 }
